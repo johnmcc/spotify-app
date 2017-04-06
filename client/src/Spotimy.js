ede8e4541fb93497adb072f04ce00ef460f10ab2
John McCollum <johnvmccollum@gmail.com>
2017-04-06 16:24:27 +0100

    Refactored Spotimy.js to use ES6

--- client/src/Spotimy.js
+++ client/src/Spotimy.js
@@ -1,30 +1,33 @@
-var Spotimy = function(){
-  this.clientId = "24f59f279b824fe9891b49c10c9e6130";
-  this.redirect_uri = "http://spotimy.com:3000";
+export default class Spotimy {
+  constructor(){
+    this.clientId = "24f59f279b824fe9891b49c10c9e6130";
+    this.redirect_uri = "http://spotimy.com:3000";
 
-  var params = this.getHashParams();
-  this.accessToken = params.access_token || null;
-};
+    var params = this.getHashParams();
+    this.accessToken = params.access_token || null;    
+  }
 
-Spotimy.prototype = {
-  getAuthURL: function(){
-      var url = "https://accounts.spotify.com/authorize?";
-      url += "client_id=" + this.clientId;
-      url += "&response_type=token";
-      url += "&scope=user-read-recently-played";
-      url += "&redirect_uri=" + this.redirect_uri;
+  getAuthURL(){
+    var url = "https://accounts.spotify.com/authorize?";
+    url += "client_id=" + this.clientId;
+    url += "&response_type=token";
+    url += "&scope=user-read-recently-played";
+    url += "&redirect_uri=" + this.redirect_uri;
 
-      return url;
-  },
-  getRecentlyPlayedUrl: function(){
+    return url;
+  }
+
+  getRecentlyPlayedUrl() {
     var url = "https://api.spotify.com/v1/me/player/recently-played";
     return url;
-  },
-  getSongInfoUrl: function(ids){
+  }
+
+  getSongInfoUrl(ids){
     var url = "https://api.spotify.com/v1/audio-features/?ids=" + ids;
     return url;
-  },
-  getHashParams: function() {
+  }
+
+  getHashParams(){
     var hashParams = {};
     var e, r = /([^&;=]+)=?([^&;]*)/g,
       q = window.location.hash.substring(1);
@@ -33,6 +36,4 @@ Spotimy.prototype = {
     }
     return hashParams;
   }
-};
-
-module.exports = Spotimy;
+}
