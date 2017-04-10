b6c140cf505f6f51333db2f3fe16d0176d7398ad
John McCollum <johnvmccollum@gmail.com>
2017-04-10 15:36:01 +0100

    Refactored infobox

--- client/src/components/InfoBox.jsx
+++ client/src/components/InfoBox.jsx
@@ -2,56 +2,36 @@ import React from 'react';
 import { Chart } from 'react-google-charts';
 
 const InfoBox = ({selectedSong, selectedSongInfo}) => {
-  const getTrackName = () => {
-    if(selectedSong){
-      return selectedSong.name;
-    }
-  }
+  if(!selectedSong) return null;
 
   const getArtists = () => {
-    if(selectedSong){
-      return selectedSong.artists.map(artist => { return artist.name }).join(" / ");
-    }
-  }
-
-  const getImage = () => {
-    if(selectedSong){
-      return selectedSong.album.images[0].url;
-    }
-  }
-
-  const getAlbumName = () => {
-    if(selectedSong){
-      return selectedSong.album.name;
-    }
+    return selectedSong.artists.map(artist => { return artist.name }).join(" / ");
   }
 
   const getData = () => {
-    if(selectedSongInfo){
-      var danceability = parseInt(selectedSongInfo.danceability * 100, 10);
-      var valence = parseInt(selectedSongInfo.valence * 100, 10);
-      var energy = parseInt(selectedSongInfo.energy * 100, 10);
+    var danceability = parseInt(selectedSongInfo.danceability * 100, 10);
+    var valence = parseInt(selectedSongInfo.valence * 100, 10);
+    var energy = parseInt(selectedSongInfo.energy * 100, 10);
 
-      return [ 
-        ['Danceability', 'Happiness', 'Energy'], 
-        [danceability, valence, energy]
-      ];
-    }
+    return [ 
+      ['Danceability', 'Happiness', 'Energy'], 
+      [danceability, valence, energy]
+    ];
   }
 
   const infoBoxStyle = {
-    backgroundImage: 'url(' + getImage() + ')',
+    backgroundImage: 'url(' + selectedSong.album.images[0].url + ')',
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
     backgroundPosition: '50% 50%'
   };
 
-  const elements = (
+  return (
     <div id="infoBox" style={ infoBoxStyle }>
       <div id="infoTextWrap">
-        <h2>{ getTrackName() }</h2>
+        <h2>{ selectedSong.name }</h2>
         <h3>By { getArtists() }</h3>
-        <h4>From the album { getAlbumName() }</h4>
+        <h4>From the album { selectedSong.album.name }</h4>
 
         <Chart
           chartType="Gauge"
@@ -68,8 +48,6 @@ const InfoBox = ({selectedSong, selectedSongInfo}) => {
       </div>
     </div>
   );
-
-  return selectedSong ? elements:null;
 }
 
 export default InfoBox;
