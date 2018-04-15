5941e910adec80639fed6c10f6006f58a895d95c
John McCollum <johnvmccollum@gmail.com>
2018-04-15 23:17:40 +0100

    DEFINITELY the last attempt for now :(

--- client/src/components/MainRouter.jsx
+++ client/src/components/MainRouter.jsx
@@ -7,21 +7,20 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 const MainRouter = () => {
   var spotimy = new Spotimy();
-  let hashHistory = Router.hashHistory;
 
   return (
-    <Router history={ hashHistory }>
+    <Router history={browserHistory} basename={'spotify-app'}>
       <div>
         <div id="titlebar">
           <h1>Spotimy</h1>
           <ul>
-            <li><Link to="/spotify-app/">Home</Link></li>
-            <li><Link to="/spotify-app/about">About</Link></li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
           </ul>
         </div>
 
-        <Route exact path="/spotify-app/" render={() => <App spotimy={spotimy} /> } />
-        <Route path="/spotify-app/about" component= { About } />
+        <Route exact path="/" render={() => <App spotimy={spotimy} /> } />
+        <Route path="/about" component= { About } />
       </div>
     </Router>
   );
